38c81a7ad4ec7c597dd269e3cf95cd33f1f31dde
pilotessa <pilotessa@gmail.com>
2017-06-12 10:03:19 +0300

    Rollback TaskStatusEnum changes
    There is no reason to use maps with primitive keys

--- src/js/models/TaskStatusEnum.js
+++ src/js/models/TaskStatusEnum.js
@@ -6,21 +6,18 @@ export default (function () {
         }
     }
 
-    const codes = new Map();
-    codes.set('ACTIVE_TASK', new TaskStatus('ACTIVE_TASK', 'Active'));
-    codes.set('COMPLETED_TASK', new TaskStatus('COMPLETED_TASK', 'Completed'));
-
-    function getByCode(code) {
-        if (codes.has(code)) {
-            return codes.get(code);
-        } else {
-            throw new Error("Task Code is not found");
-        }
-    }
+    const ACTIVE_TASK = new TaskStatus('ACTIVE_TASK', 'Active');
+    const COMPLETED_TASK = new TaskStatus('COMPLETED_TASK', 'Completed');
 
     return {
-        ACTIVE_TASK: getByCode('ACTIVE_TASK'),
-        COMPLETED_TASK: getByCode('COMPLETED_TASK'),
-        getByCode
+        ACTIVE_TASK,
+        COMPLETED_TASK,
+        getByCode(code) {
+            if (code in this) {
+                return this[code];
+            } else {
+                throw new Error("Task Code is not found");
+            }
+        }
     }
 })();
